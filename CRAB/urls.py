db9c33367e49e5b5784c7b83868cb26910949045
StalloneDev <139977747+StalloneDev@users.noreply.github.com>
2024-11-28 00:34:31 +0100

    Update urls.py

--- CRAB/urls.py
+++ CRAB/urls.py
@@ -22,6 +22,4 @@ from django.conf import settings
 urlpatterns = [
     path('',  include('Demo.urls')),
     path('admin/', admin.site.urls),
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
